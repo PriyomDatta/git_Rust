6f590d9f5b67b2e221853bc906da7041ce2a2983
PriyomDatta <priyomdat@gmail.com>
2024-12-16 12:08:43 +0530

    Update main.rs
    Need to check 'cargo run', 'cargo build' and 'cargo test'

--- Proj_001/src/main.rs
+++ Proj_001/src/main.rs
@@ -15,3 +15,16 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 fn test_gcd(){
     assert_eq!(gcd(14,15),1);
 }
+
+#[test]
+fn test_gcd_1(){
+    assert_eq!(gcd(12,15),3);
+}
+fn main() {
+    let mut x: i32 = 10;
+    x = x - 12;
+    println!("Hello, world! {}", x);
+    
+    let res = gcd(10, 12);
+    println!("Res is {}", res);
+}
